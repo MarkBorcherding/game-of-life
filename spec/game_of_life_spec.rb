26969ed37dd39061bb29026062641969b22fa599
Mark Borcherding <markborcherding@gmail.com>
2013-06-11 22:55:43 -0500

    initial commit

---
+++ spec/game_of_life_spec.rb
@@ -0,0 +1,114 @@
+require './src/game_of_life'
+
+describe 'Game of life' do
+
+  describe World do
+
+    let(:height) { 4 }
+    let(:width) { 5 }
+    let(:world) { World.new height, width }
+
+    subject { world }
+
+    its(:height) { should == 4 }
+    its(:width ) { should == 5 }
+
+    describe 'default cell state' do
+      it { world.alive?(0,0).should == false }
+    end
+
+    describe 'live!' do
+      before { world.live! 1,1 }
+      it { world.alive?(1,1).should == true }
+    end
+
+    describe 'dead things are not alive' do
+      before { world.live! 1,1 }
+      it { world.dead?(1,1).should == false }
+    end
+
+    describe '#neighbors' do
+
+      describe 'max neighbors' do
+
+        before do
+          height.times do |h|
+            width.times do |w|
+              world.live! h,w
+            end
+          end
+        end
+
+        describe 'corner cell' do
+          it { world.living_neighbors(0,0).should == 2 }
+        end
+
+        describe 'edge cell' do
+          it { world.living_neighbors(0,1).should == 3 }
+        end
+
+        describe 'middle cell' do
+          it { world.living_neighbors(1,1).should == 4 }
+        end
+
+      end
+
+    end
+
+    describe 'under-populated' do
+
+      before do
+        world.live! 0,0
+        world.live! 0,1
+      end
+
+      it { world.over_populated?(0,1).should == true }
+
+    end
+
+    describe 'surviving' do
+
+      describe 'with two neighbors' do
+        before do
+          world.live! 0,0
+          world.live! 0,1
+          world.live! 0,2
+        end
+
+        it { world.surviving?(0,1).should == true }
+      end
+
+      describe 'with three neighbors' do
+        before do
+          world.live! 0,0
+          world.live! 0,1
+          world.live! 0,2
+          world.live! 1,1
+        end
+        it { world.surviving?(0,1).should == true }
+      end
+    end
+
+    describe 'dies' do
+      before do
+        world.live! 0,1
+        world.live! 1,0
+        world.live! 1,1
+        world.live! 1,2
+        world.live! 2,1
+      end
+      it { world.dies?(1,1).should == true }
+    end
+
+    describe 'reporoduces' do
+      before do
+        world.live! 0,0
+        world.live! 0,2
+        world.live! 1,1
+      end
+      it { world.reproduces?(0,1).should == true }
+    end
+
+  end
+
+end
---
+++ src/game_of_life.rb
@@ -0,0 +1,76 @@
+class World
+
+  attr_accessor :height, :width, :cells
+
+  def initialize(height, width)
+    self.height = height
+    self.width = width
+    self.cells = Array.new height * width
+  end
+
+
+  def dead?(row, col)
+    !alive? row, col
+  end
+
+  def alive?(row, col)
+    cell(row, col) == :alive
+  end
+
+  def live!(row, col)
+    cells[index row, col] = :alive
+  end
+
+  def over_populated?(row, col)
+    alive?(row, col) and living_neighbors(row, col) < 2
+  end
+
+  def surviving?(row, col)
+    alive?(row, col) and living_neighbors(row, col).between?(2,3)
+  end
+
+  def dies?(row,col)
+    alive?(row, col) and living_neighbors(row, col) == 4
+  end
+
+  def reproduces?(row, col)
+    dead?(row, col) and living_neighbors(row, col) == 3
+  end
+
+  def cell(row, col)
+    cells[index row, col]
+  end
+
+  def living_neighbors(row, col)
+    neightbors(row, col).count { |cell| cell == :alive }
+  end
+
+  def neightbors(row, col)
+    [above(row, col),
+     left(row, col),
+     right(row, col),
+    below(row, col)]
+  end
+
+  def index(row, col)
+    row * height + col
+  end
+
+  def above(row, col)
+    cell(row-1, col) unless row == 0
+  end
+
+  def below(row, col)
+    cell(row+1,col) unless row == @height - 1
+  end
+
+  def left(row, col)
+    cell(row, col-1) unless col == 0
+  end
+
+  def right(row, col)
+    cell(row, col+1) unless col == @width - 1
+  end
+
+
+end
